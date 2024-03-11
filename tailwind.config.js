aa845c9de34efdfbfbd734fbe5023a2f9d2ddd9f
Paul Sanders <psanders1@gmail.com>
2024-03-11 12:53:53 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#317350",
+        secondary: "#86B99D",
+      },
+    },
+  },
+  plugins: [],
+};
